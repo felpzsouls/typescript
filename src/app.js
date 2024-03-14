471cbc2f616562498389c34914aba9f48769342c
felpzsouls <felipedark2509@gmail.com>
2024-03-14 12:54:11 -0300

    i learn number and bigint

--- src/app.js
+++ src/app.js
@@ -45,6 +45,16 @@ else {
     console.log('tarefa pendente');
 }
 ;
+// => number
+let num1 = 23; //inteiro
+let num2 = 0x78FC; //hexadecimal
+let num3 = 0o577; //octal
+let num4 = 0b1101; //binario
+//  bigint 
+let big1 = 2934857902347590347590n;
+let big2 = 268435457n;
+let big3 = 0x2000000000003n;
+let big4 = 274877906947n;
 //consoles
 console.log(numbers(1, 5));
 console.log(message);
@@ -55,3 +65,17 @@ console.log(nome);
 console.log(calc(1, 3));
 console.log(tarefaConcluida);
 console.log(tarefaPendente);
+console.log('number - ponto flutuante:', num1);
+console.log('number - hexadecimal:', num2);
+console.log(typeof (num2));
+console.log('number - octal:', num3);
+console.log(typeof (num3));
+console.log('number - binario:', num4);
+console.log(typeof (num4));
+console.log('number - ponto flutuante:', big1);
+console.log('number - hexadecimal:', big2);
+console.log(typeof (big2));
+console.log('number - octal:', big3);
+console.log(typeof (big3));
+console.log('number - binario:', big4);
+console.log(typeof (big4));
--- src/app.ts
+++ src/app.ts
@@ -57,8 +57,23 @@ if (concluido) {
     console.log('tarefa pendente')
 };
 
+// => number
+
+let num1: number = 23; //inteiro
+let num2: number = 0x78FC; //hexadecimal
+let num3: number = 0o577; //octal
+let num4: number = 0b1101; //binario
+
+//  bigint 
+let big1: bigint = 2934857902347590347590n;
+let big2: bigint = 0b10000000000000000000000000001n;
+let big3: bigint = 0x2000000000003n;
+let big4: bigint = 0o4000000000003n;
+
+
 //consoles
 console.log(numbers(1, 5));
+
 console.log(message);
 console.log(mult(2, 5));
 console.log(carro);
@@ -67,3 +82,19 @@ console.log(nome);
 console.log(calc(1, 3));
 console.log(tarefaConcluida);
 console.log(tarefaPendente);
+
+console.log('number - ponto flutuante:', num1);
+console.log('number - hexadecimal:', num2);
+console.log(typeof(num2));
+console.log('number - octal:', num3);
+console.log(typeof(num3));
+console.log('number - binario:', num4);
+console.log(typeof(num4));
+
+console.log('number - ponto flutuante:', big1);
+console.log('number - hexadecimal:', big2);
+console.log(typeof(big2));
+console.log('number - octal:', big3);
+console.log(typeof(big3));
+console.log('number - binario:', big4);
+console.log(typeof(big4));
