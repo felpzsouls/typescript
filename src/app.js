7862b749b18da98de5eb0d454982f9da78229332
felpzsouls <felipedark2509@gmail.com>
2024-03-14 11:26:14 -0300

    typeannotation trainig

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 "use strict";
 let message = 'Salve Mundo';
+let number = 8;
+let vdd = true;
+function numbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(numbers(1, 5));
 console.log(message);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 let message: string = 'Salve Mundo'
+let number: number = 8;
+let vdd: boolean = true;
 
-console.log(message)
+function numbers(num1: number, num2: number) {
+    return num1 + num2
+}
+
+console.log(numbers(1, 5));
+console.log(message);
