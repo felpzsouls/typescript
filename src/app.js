8af4ad4a9326adff5696218ea2055a78934c9dcd
felpzsouls <felipedark2509@gmail.com>
2024-03-14 11:43:09 -0300

    learn more

--- src/app.js
+++ src/app.js
@@ -5,5 +5,25 @@ let vdd = true;
 function numbers(num1, num2) {
     return num1 + num2;
 }
+// => Variaveis
+let nome = "Felpz";
+// => Arrays
+let animais = ["cachorro", "gato", "coelho", 'macaco'];
+// => objetos
+let carro;
+carro = {
+    nome: 'toyota',
+    ano: 2019,
+    preco: 80000
+};
+// => funcoes
+function mult(num1, num2) {
+    return num1 * num2;
+}
+//consoles
 console.log(numbers(1, 5));
 console.log(message);
+console.log(mult(2, 5));
+console.log(carro);
+console.log(animais);
+console.log(nome);
--- src/app.ts
+++ src/app.ts
@@ -6,5 +6,37 @@ function numbers(num1: number, num2: number) {
     return num1 + num2
 }
 
+// => Variaveis
+
+let nome: string = "Felpz"
+
+// => Arrays
+let animais: string[] = ["cachorro", "gato", "coelho", 'macaco']
+
+// => objetos
+
+let carro: {
+    nome: string;
+    ano: number;
+    preco: number;
+};
+
+carro = {
+    nome: 'toyota',
+    ano: 2019,
+    preco: 80000
+}
+
+// => funcoes
+
+function mult(num1: number, num2: number) {
+    return num1 * num2
+}
+
+//consoles
 console.log(numbers(1, 5));
 console.log(message);
+console.log(mult(2, 5))
+console.log(carro)
+console.log(animais)
+console.log(nome)
